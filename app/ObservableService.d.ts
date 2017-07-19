8e125dc9b6ff6a7ae45c218ccb86c920ccb83041
Jonathan Bass <jonathanbass@dimensions.co.uk>
2017-07-19 08:06:05 +0100

    removed unwanted typings file

--- app/ObservableService.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="jquery" />
-declare class ObservableService {
-    UpdateGreeting: (newGreeting: string) => void;
-    Deferred: JQueryDeferred<string>;
-    Subscribe: () => JQueryDeferred<string>;
-    constructor();
-}
