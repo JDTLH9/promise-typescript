043b05f647519fefc5b770d9d9b91cbe0b8d3c2c
Jonathan Bass <jonathanbass@dimensions.co.uk>
2017-07-19 08:47:00 +0100

    ignored TypeScript output files

--- app/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-$(function () {
-    var observableService = new ObservableService();
-    var classTwo = new ClassTwo(observableService);
-    var classOne = new ClassOne(observableService);
-    $("#my-button").click(function () {
-        classOne.UpdateGreeting($("#my-text").val());
-    });
-});
